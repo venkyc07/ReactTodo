997157bc8a3c951bdf42056d0dd94812ff28fa80
venkyc07 <venkatesh.chidurala20@gmail.com>
2016-09-21 15:03:09 +0530

    add static data

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,27 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+          {
+            id: 1,
+            text: 'do your Work'
+          },
+          {
+            id: 2,
+            text: 'Take a cup of milk'
+          },
+      ]
+    };
+  },
   render: function() {
+    var {todos} = this.state;
+
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
